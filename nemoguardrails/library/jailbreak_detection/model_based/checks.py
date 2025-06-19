9a180a688d17d625ee7493ee1d474f4adbc20ca9
Pouyanpi <13303554+Pouyanpi@users.noreply.github.com>
2025-06-19 10:39:58 +0200

    style: apply pre-commits

--- nemoguardrails/library/jailbreak_detection/model_based/checks.py
+++ nemoguardrails/library/jailbreak_detection/model_based/checks.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import logging
+import os
 from functools import lru_cache
 from pathlib import Path
 from typing import Union
