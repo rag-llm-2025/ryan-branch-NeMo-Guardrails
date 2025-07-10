7c9feea422d2c81d224a8b72d40656617d42ff7a
s1ryaniu <ryan.niu@cerence.com>
2025-07-10 10:10:04 +0800

    fix: use ubuntu instead of

--- ryan_bot/env_setup/check_env_config.sh
+++ ryan_bot/env_setup/check_env_config.sh
@@ -22,7 +22,7 @@ curr_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source $curr_dir/update_env_host.sh
 
 # load .env config
-source $curr_dir/.env.$USER
+source $curr_dir/.env.$(whoami)
 print_header "当前环境变量文件"
 printf "%-20s: %s\n" "$curr_dir/.env.$(whoami)"
 print_separator
--- ryan_bot/env_setup/update_env_host.sh
+++ ryan_bot/env_setup/update_env_host.sh
@@ -2,15 +2,9 @@
 
 # 更新.env.ryan_niu文件中的HOST值
 curr_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-ENV_FILE=$curr_dir/.env.$USER
-echo "machine name: $USER@$HOSTNAME"
+ENV_FILE=$curr_dir/.env.$(whoami)
+echo "machine name: $(whoami)@$HOSTNAME"
 echo "env file: $ENV_FILE"
-
-# 检查当前用户是否为ryan_niu且主机名不是gn403
-if [ "$USER" != "ryan_niu" ] && [ "$HOSTNAME" != "gn403" ]; then
-    echo "当前用户不是ryan_niu, 且hostname不是gn403时, 脚本终止"
-    exit 0
-fi
 
 # 获取当前主机的IP地址（排除127.0.0.1）
 NEW_HOST=$(nslookup $HOSTNAME | grep -oP 'Address:\s*\K(?!127\.0\.)[\d\.]+')
@@ -21,6 +15,10 @@ if [ -z "$NEW_HOST" ]; then
     exit 1
 fi
 
-# 使用sed命令替换HOST行
-sed -i "s/^HOST=.*/HOST=$NEW_HOST/" "$ENV_FILE"
-echo "已将.env.ryan_niu中的HOST更新为: $NEW_HOST"
+# 检查当前用户是为ryan_niu且主机名是gn403
+if [ "$(whoami)" == "ryan_niu" ] && [ "$HOSTNAME" == "gn403" ]; then
+    echo "当前用户是ryan_niu, 且hostname是gn403时, 替换HOST为: $NEW_HOST"
+    # 使用sed命令替换HOST行
+    sed -i "s/^HOST=.*/HOST=$NEW_HOST/" "$ENV_FILE"
+    echo "已将.env.ryan_niu中的HOST更新为: $NEW_HOST"
+fi
