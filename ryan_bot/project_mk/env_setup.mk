fa77d808818ce14d5174323092bd480d3af56a02
s1ryaniu <ryan.niu@cerence.com>
2025-07-17 14:21:53 +0800

    feat: optimize the structure of makefile targets

--- ryan_bot/project_mk/env_setup.mk
+++ ryan_bot/project_mk/env_setup.mk
@@ -1,9 +1,40 @@
 # env_setup.mk - 环境设置相关命令
 
-.PHONY: setup_env_from_scratch update_env_host print_structure
+.PHONY: setup_env_from_scratch update_yaml_config install_dependencies print_env check_env update_env_host print_structure
 
 setup_env_from_scratch: print_structure install_dependencies update_env_host check_cuda_and_run update_yaml_config check_env
 	@echo "Done! 完成运行环境部署"
+
+update_yaml_config:
+	@echo "更新YAML配置..."
+	cd $(LLM_DIR)/src/nemo-guardrails/ryan_bot/ && \
+	python3 -c "from utils.helper import yml_config_update; yml_config_update('./config/qwen_model/config.yml', '${ENGINE_NAME}', '${MODEL_NAME}', '${MODEL_PATH}', '${DEVICE}', '${CHECKPOINT_PATH}')"
+
+install_dependencies:
+	@echo "安装依赖..."
+	cd $(LLM_DIR)/src/nemo-guardrails/ && pip3 install -e .
+	cd $(LLM_DIR)/src/nemo-guardrails/ryan_bot/ && pip3 install -r requirements.txt
+
+print_env:
+	@printf "\033[1;36m=== .env文件导入环境变量 ===\033[0m\n"
+	@printf "  \033[1;35mUSER:\033[0m $(USER)\n"
+	@printf "  \033[1;35mHOST:\033[0m $(HOST)\n"
+	@printf "  \033[1;35mPORT:\033[0m $(PORT)\n"
+	@printf "  \033[1;35mLLM_DIR:\033[0m $(LLM_DIR)\n"
+	@printf "  \033[1;35mMODEL_NAME:\033[0m $(MODEL_NAME)\n"
+	@printf "  \033[1;35mMODEL_PATH:\033[0m $(MODEL_PATH)\n"
+	@printf "  \033[1;35mDEVICE:\033[0m $(DEVICE)\n"
+	@printf "  \033[1;35mCHECKPOINT_PATH:\033[0m $(CHECKPOINT_PATH)\n"
+	@printf "  \033[1;35mAPI_KEY:\033[0m $(API_KEY)\n"
+	@printf "  \033[1;35mAPI_SECRET:\033[0m $(API_SECRET)\n"
+	@printf "\033[1;36m=== 项目配置开关 ===\033[0m\n"
+	@printf "  \033[1;35mSTREAM:\033[0m $(STREAM)\n"
+	@printf "  \033[1;35mENABLE_VLLM:\033[0m $(ENABLE_VLLM)\n"
+	@printf "  \033[1;35mENABLE_TEXT_EMBEDDING:\033[0m $(ENABLE_TEXT_EMBEDDING)\n"
+	@printf "  \033[1;35mENABLE_LATENCY_OPTIMIZATION:\033[0m $(ENABLE_LATENCY_OPTIMIZATION)\n"
+
+check_env:
+	cd $(LLM_DIR)/src/nemo-guardrails/ryan_bot/ && python3 -c "import nemoguardrails; print(nemoguardrails.__version__)"
 
 update_env_host:
 	@echo "machine name: $(USER)@$(HOSTNAME)"
--- ryan_bot/project_mk/utils.mk
+++ ryan_bot/project_mk/utils.mk
@@ -1,6 +1,6 @@
 # utils.mk - 工具类命令
 
-.PHONY: check_cuda_and_run update_yaml_config install_dependencies print_env check_env export_history
+.PHONY: check_cuda_and_run export_history
 
 check_cuda_and_run:
 	@echo "检查CUDA可用性..."
@@ -15,37 +15,6 @@ check_cuda_and_run:
 		echo "CUDA is not available. Using CPU."; \
 		export DEVICE="cpu"; \
 	fi
-
-update_yaml_config:
-	@echo "更新YAML配置..."
-	cd $(LLM_DIR)/src/nemo-guardrails/ryan_bot/ && \
-	python3 -c "from utils.helper import yml_config_update; yml_config_update('./config/qwen_model/config.yml', '${ENGINE_NAME}', '${MODEL_NAME}', '${MODEL_PATH}', '${DEVICE}', '${CHECKPOINT_PATH}')"
-
-install_dependencies:
-	@echo "安装依赖..."
-	cd $(LLM_DIR)/src/nemo-guardrails/ && pip install -e .
-	cd $(LLM_DIR)/src/nemo-guardrails/ryan_bot/ && pip install -r requirements.txt
-
-print_env:
-	@printf "\033[1;36m=== .env文件导入环境变量 ===\033[0m\n"
-	@printf "  \033[1;35mUSER:\033[0m $(USER)\n"
-	@printf "  \033[1;35mHOST:\033[0m $(HOST)\n"
-	@printf "  \033[1;35mPORT:\033[0m $(PORT)\n"
-	@printf "  \033[1;35mLLM_DIR:\033[0m $(LLM_DIR)\n"
-	@printf "  \033[1;35mMODEL_NAME:\033[0m $(MODEL_NAME)\n"
-	@printf "  \033[1;35mMODEL_PATH:\033[0m $(MODEL_PATH)\n"
-	@printf "  \033[1;35mDEVICE:\033[0m $(DEVICE)\n"
-	@printf "  \033[1;35mCHECKPOINT_PATH:\033[0m $(CHECKPOINT_PATH)\n"
-	@printf "  \033[1;35mAPI_KEY:\033[0m $(API_KEY)\n"
-	@printf "  \033[1;35mAPI_SECRET:\033[0m $(API_SECRET)\n"
-	@printf "\033[1;36m=== 项目配置开关 ===\033[0m\n"
-	@printf "  \033[1;35mSTREAM:\033[0m $(STREAM)\n"
-	@printf "  \033[1;35mENABLE_VLLM:\033[0m $(ENABLE_VLLM)\n"
-	@printf "  \033[1;35mENABLE_TEXT_EMBEDDING:\033[0m $(ENABLE_TEXT_EMBEDDING)\n"
-	@printf "  \033[1;35mENABLE_LATENCY_OPTIMIZATION:\033[0m $(ENABLE_LATENCY_OPTIMIZATION)\n"
-
-check_env:
-	cd $(LLM_DIR)/src/nemo-guardrails/ryan_bot/ && python3 -c "import nemoguardrails; print(nemoguardrails.__version__)"
 
 export_history:
 	@echo "正在导出命令历史记录..."
