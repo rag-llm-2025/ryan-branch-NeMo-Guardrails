8aa8c1b39bcc15662d35443fc3b5906453b638ec
s1ryaniu <ryan.niu@cerence.com>
2025-08-08 06:12:31 -0400

    feat: integrate gpt-4o for llm generation

--- ryan_bot/vllm_qwen/api_client.py
+++ ryan_bot/vllm_qwen/api_client.py
@@ -3,14 +3,18 @@ from typing import Optional
 import requests
 import time
 import os
+import json
 from requests.exceptions import RequestException
 from nemoguardrails.logging.verbose import console
 
 from simple_logger import RyanLogger as logger
+from gpt_4o_verify import GPT4OWrapper
+from generate_prompt import generate_response_prompt, generate_guardrail_prompt
 
 # api_server address and port
 RAILS_HOST = os.getenv("RAILS_HOST", "localhost")
 print("RAILS_HOST:", RAILS_HOST)
+API_KEY = os.getenv("OPENAI_URL_AUTH", "")
 
 API_URL = f"http://{RAILS_HOST}:8010/generate"
 HEADERS = {"Content-Type": "application/json"}
@@ -51,11 +55,35 @@ if __name__ == "__main__":
             if query.lower() == 'quit':
                 break
 
+            rails_start_time = time.time()
             result = send_request(query)
+            rails_cost_time = (time.time() - rails_start_time) * 1000
+
+            user_safety = "none"
+            safety_categories = ""
             if result:
-                logger.info(f"Bot: {result}")
+                logger.info(f"Bot: {result} (latency={rails_cost_time:.2f}ms)")
+                # continue
+                safety_info = json.loads(result['test_user'])
+
+                user_safety = safety_info["User Safety"]
+                safety_categories = safety_info["Safety Categories"]
             else:
                 logger.error("Error! request failed, please check the connection or logs")
+                continue
+
+            wrapper = GPT4OWrapper(API_KEY)
+            prompt = generate_response_prompt(
+                    query, user_safety, safety_categories
+                )
+            response, gpt_cost_time = wrapper.chat_completion(
+                [{"role": "user", "content": prompt}]
+            )
+            content = wrapper._process_response(response)
+            logger.info(
+                f"GPT response: {content} (latency={gpt_cost_time:.2f}ms)"
+            )
+            logger.info(f"E2E latency: {rails_cost_time + gpt_cost_time:.2f}ms")
         except KeyboardInterrupt:
             logger.info("\nOver!")
             break
